223596fe355fcd4a8a06d52786db8758f46349f0
23Vatsal <vatsalbhatnagar007@gmail.com>
2020-04-23 13:19:41 +0530

    First Commit

---
+++ javaChatBot/src/main/java/com/luv2code/javaChatBot/AddAIML.java
@@ -0,0 +1,40 @@
+package com.luv2code.javaChatBot;
+
+import java.io.File;
+import org.alicebot.ab.Bot;
+import org.alicebot.ab.MagicBooleans;
+
+public class AddAIML {
+	
+	private static final boolean TRACE_MODE = false;
+    static String botName = "super";
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 try {
+			 
+	            String resourcesPath = getResourcesPath();
+	            System.out.println(resourcesPath);
+	            MagicBooleans.trace_mode = TRACE_MODE;
+	            Bot bot = new Bot("super", resourcesPath);
+	             
+	            bot.writeAIMLFiles();
+	 
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	    }
+	 
+	    private static String getResourcesPath() {
+	        File currDir = new File(".");
+	        String path = currDir.getAbsolutePath();
+	        path = path.substring(0, path.length() - 2);
+	        System.out.println(path);
+	        String resourcesPath = path + File.separator + "src" + File.separator + "main" + File.separator + "resources";
+	        return resourcesPath;
+		
+
+	}
+
+}
